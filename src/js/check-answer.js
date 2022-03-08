5f7900aa913ca5b6aba32b62f2023966d5dff92b
acsmit44 <50591795+acsmit44@users.noreply.github.com>
2022-03-07 19:52:03 -0700

    Created more functions for the game part

--- src/js/check-answer.js
+++ src/js/check-answer.js
@@ -1,7 +1,24 @@
+const newIcon = function(iconName, status) {
+  /* create icon wrapper */
+  const div = document.createElement('div');
+  div.classList.add('icon-wrapper', status, 'd-flex', 'align-items-center', 'justify-content-center');
+
+  /* create the icon */
+  const i = document.createElement('i');
+  i.classList.add('bi', `bi-${iconName}`);
+
+  div.appendChild(i);
+  return div;
+}
+
+const isSubset = function(A, B) {
+  return (![...A].some((x) => !B.has(x)));
+}
+
 const setDif = function(A, B) {
   return new Set([...A].filter((x) => !B.has(x)));
 }
 
-const newProgressImg = function() {
-
-};
+const setIntersection = function(A, B) {
+  return new Set([...A].filter((x) => B.has(x)));
+}
