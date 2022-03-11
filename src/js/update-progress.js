7296c10344a3312197df151fa7cd8d0964b14e27
acsmit44 <50591795+acsmit44@users.noreply.github.com>
2022-03-10 20:20:38 -0700

    Removed redundant function, moved some code

--- src/js/update-progress.js
+++ src/js/update-progress.js
@@ -1,77 +1,32 @@
-const updateProgressSet = function (valArr, type, valSet) {
+const updateProgressSet = function (valArr, set, type) {
   const correctStudios = new Set(window.anime[type]);
   valArr.forEach((val) => {
     if (!correctStudios.has(val)) {
-      valSet.delete(val);
+      set.delete(val);
     }
   });
 };
 
-const updateStudios = function (studios) {
-  updateProgressSet(studios, 'studios', possibleStudios);
-};
-
-const updateProgressGroup = function (val, status, type, valSet) {
+const updateProgressGroup = function (val, status, set, type) {
   const rootGroup = document.getElementById(type);
   if (status === 'correct') {
-    valSet.delete(val);
-    [...valSet].forEach((val) => {
+    set.delete(val);
+    [...set].forEach((val) => {
       const btn = rootGroup.querySelector(`#${val}`);
       btn.classList.add('btn-secondary', 'd-none', 'd-md-inline-block');
       btn.classList.remove('btn-primary');
       btn.disabled = true;
     });
-    valSet.clear();
-    valSet.add(val);
+    set.clear();
+    set.add(val);
     rootGroup.querySelector(`#${val}`).classList.add('btn-success');
   } else {
     const btn = rootGroup.querySelector(`#${val}`);
     btn.classList.add('btn-secondary', 'd-none', 'd-md-inline-block');
     btn.classList.remove('btn-primary');
     btn.disabled = true;
-    valSet.delete(val);
+    set.delete(val);
   }
-};
-
-const updateSources = function (source, status) {
-  updateProgressGroup(source, status, 'sources', sources);
-};
-
-const updateFormats = function (format, status) {
-  updateProgressGroup(format, status, 'formats', formats);
-};
-
-const updateSeasons = function (season, status) {
-  updateProgressGroup(season, status, 'seasons', seasons);
-};
-
-/**
- * Uses a progress object and type to update a progress bar.
- * @param {Object} progressObj - The object used to track progress
- * @param {string} type - A string used to find the elements on the page
- */
-const updateProgressBar = function (progressObj, type) {
-  const min = progressObj.min;
-  const max = progressObj.max;
-  const total = (max - min);
-  const low = progressObj.low;
-  const high = progressObj.high;
-
-  document.getElementById(`${type}-low`).textContent = low;
-  document.getElementById(`${type}-high`).textContent = high;
-
-  const start = document.getElementById(`${type}-progress-left`);
-  const middle = document.getElementById(`${type}-progress-middle`);
-  const end = document.getElementById(`${type}-progress-right`);
-
-  start.setAttribute('aria-valuenow', `${low - min}`);
-  start.style.width = `${((low - min) / total) * 100}%`;
-
-  middle.setAttribute('aria-valuenow', `${high - low}`);
-  middle.style.width = `${((high - low) / total) * 100}%`;
-
-  end.setAttribute('aria-valuenow', `${max - high}`);
-  end.style.width = `${((max - high) / total) * 100 }%`;
 };
 
 /**
@@ -95,19 +50,49 @@ const updateNumRange = function (newVal, dif, threshold, obj, key) {
       obj.high = Math.min(obj.high, newVal - 1);
     }
   }
+
+  const { min, max, low, high } = obj;
+  const total = (max - min);
+
+  document.getElementById(`${key}-low`).textContent = low;
+  document.getElementById(`${key}-high`).textContent = high;
+
+  const start = document.getElementById(`${key}-progress-left`);
+  const middle = document.getElementById(`${key}-progress-middle`);
+  const end = document.getElementById(`${key}-progress-right`);
+
+  start.setAttribute('aria-valuenow', `${low - min}`);
+  start.style.width = `${((low - min) / total) * 100}%`;
+
+  middle.setAttribute('aria-valuenow', `${high - low}`);
+  middle.style.width = `${((high - low) / total) * 100}%`;
+
+  end.setAttribute('aria-valuenow', `${max - high}`);
+  end.style.width = `${((max - high) / total) * 100 }%`;
 };
 
-const updatePopRange = function (popularity, dif, threshold) {
-  updateNumRange(popularity, dif, threshold, popRange, 'popularity');
-  updateProgressBar(popRange, 'popularity');
+/*
+const progressCheckers = {
+  studios: (guessStudiosSet) => {
+    updateProgressSet(guessStudiosSet, 'studios', possibleStudios);
+  },
+  episodes: (guess, dif, threshold) => {
+    updateNumRange(guess, dif, threshold, epsRange, 'episodes');
+  },
+  year: (guess, dif, threshold) => {
+    updateNumRange(guess, dif, threshold, yearRange, 'year');
+  },
+  popularity: (guess, dif, threshold) => {
+    updateNumRange(guess, dif, threshold, popRange, 'popularity');
+  },
+  format: (formatType, status) => {
+    updateProgressGroup(formatType, status, 'formats', formats);
+  },
+  source: (sourceType, status) => {
+    updateProgressGroup(sourceType, status, 'sources', sources);
+  },
+  season: (season, status) => {
+    updateProgressGroup(season, status, 'seasons', seasons);
+  },
 };
-
-const updateEpsRange = function (numEps, dif, threshold) {
-  updateNumRange(numEps, dif, threshold, epsRange, 'episodes');
-  updateProgressBar(epsRange, 'episodes');
-};
-
-const updateYearRange = function (year, dif, threshold) {
-  updateNumRange(year, dif, threshold, yearRange, 'year');
-  updateProgressBar(yearRange, 'year');
-};
+*/
