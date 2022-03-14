9120b3507722879a869c9a6c26de16b54723765d
acsmit44 <50591795+acsmit44@users.noreply.github.com>
2022-03-13 20:26:43 -0700

    Added query to get daily anime

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
 const PORT = process.env.PORT || 5000;
 
 app.use(express.static(path.join(__dirname, 'static/')));
 
 app
   .get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')))
+  .get('/db', async (req, res) => {
+    try {
+      const client = await pool.connect();
+      const result = await client.query('SELECT * FROM DailyAnime');
+      const results = { 'results': (result) ? result.rows[0] : null};
+      const jsonData = JSON.stringify(results);
+      res.send(jsonData);
+      client.release();
+    } catch (err) {
+      console.error(err);
+      res.send({error: err});
+    }
+  })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
