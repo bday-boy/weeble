c5afa4a8b405c236629b1163ba9eaead6273e9fc
acsmit44 <50591795+acsmit44@users.noreply.github.com>
2022-03-13 20:57:30 -0700

    Added quotes cuz postgresql is a little BABY

--- daily-anime.js
+++ daily-anime.js
@@ -11,7 +11,7 @@ const insertQuery = 'INSERT INTO DailyAnime VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$
 const formatArray = function (arr) {
   const newArr = []
   arr.forEach((el) => {
-    newArr.push(el.replaceAll("'", "''"));
+    newArr.push(`'${el.replaceAll("'", "''")}'`);
   });
   return `ARRAY[${newArr.join(', ')}]`;
 };
