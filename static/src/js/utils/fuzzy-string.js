c525a94fa984ad0380220c5ae386969b6c44db43
acsmit44 <acsmit44@asu.edu>
2022-05-01 18:37:17 -0700

    Switched to pre-allocated matrices

--- static/src/js/utils/fuzzy-string.js
+++ static/src/js/utils/fuzzy-string.js
@@ -1,5 +1,20 @@
+// Rather than allocating each table over and over for each function call, we
+// really only need to do it once
 const MAX_LEN_TITLE = 200;
-const faster_min = Math.min;
+const L = Array(MAX_LEN_TITLE).fill().map(() => Array(MAX_LEN_TITLE).fill(0));
+const D = Array(MAX_LEN_TITLE).fill().map(() => Array(MAX_LEN_TITLE).fill(0));
+
+// Initialize Levenshtein distance matrix
+for (let i = 0; i < MAX_LEN_TITLE; i++) {
+  D[i][0] = i;
+}
+for (let j = 0; j < MAX_LEN_TITLE; j++) {
+  D[0][j] = j;
+}
+
+// Cache Math.min in a variable to (probably) make it faster. Honestly idk if
+// this even is faster, but it's a way to speed things up in Python
+const fasterMin = Math.min;
 
 /**
  * Computes the levenshtein distance between two strings. An adjacent character
@@ -12,50 +27,37 @@ const levenshtein = function (a, b) {
   const a_len = a.length;
   const b_len = b.length;
 
-  const dist = Array(a_len + 1).fill().map(() => Array(b_len + 1).fill(0));
-
-  for (let i = 0; i < a_len + 1; i++) {
-    dist[i][0] = i;
-  }
-  for (let j = 0; j < b_len + 1; j++) {
-    dist[0][j] = j;
-  }
-
   for (let i = 1; i < a_len + 1; i++) {
     for (let j = 1; j < b_len + 1; j++) {
       const cost = (a[i - 1] !== b[j - 1]);
-      dist[i][j] = faster_min(
-        dist[i - 1][j] + 1,
-        dist[i][j - 1] + 1,
-        dist[i - 1][j - 1] + cost
+      D[i][j] = fasterMin(
+        D[i - 1][j] + 1,
+        D[i][j - 1] + 1,
+        D[i - 1][j - 1] + cost
       );
       if (
         1 < i && 1 < j &&
         a[i - 1] === b[j - 2] &&
         a[i - 2] === b[j - 1]
       ) {
-        dist[i][j] = faster_min(dist[i][j], dist[i - 2][j - 2] + 1);
+        D[i][j] = fasterMin(D[i][j], D[i - 2][j - 2] + 1);
       }
     }
   }
 
-  return dist[a_len][b_len];
+  return D[a_len][b_len];
 };
-
-
-// const L = Array(MAX_LEN_TITLE).fill().map(() => Array(MAX_LEN_TITLE).fill(0));
 
 /**
  * Computes the table of the LCS algorithm.
  * @param {string} search - The user's entry to search for in the text.
  * @param {string} text - The text to compare with the search.
- * @returns {Array<Array<int>>} A 2-dimensional array of ints.
+ * @returns {number} The length of the LCS.
  */
 const lcsTable = function (search, text) {
+  // Keep lengths in variables so we don't do attribute lookup each loop
   const searchLength = search.length;
   const textLength = text.length;
-  const L = Array(searchLength + 1).fill()
-    .map(() => Array(textLength + 1).fill(0));
 
   for (let i = 1; i <= searchLength; i++) {
     for (let j = 1; j <= textLength; j++) {
@@ -69,28 +71,19 @@ const lcsTable = function (search, text) {
     }
   }
 
-  return { L, length: L[searchLength][textLength] };
+  return L[searchLength][textLength];
 };
-
-/**
- * @typedef {Object} lcsBacktrack
- * @property {number} longest - The longest substring in the LCS.
- * @property {string} html - An HTML string where parts of the LCS are
- * surrounded by a <mark> tag.
- */
 
 /**
  * Computes the indices of the subsequence found in the text.
  * @param {string} search - The user's entry to search for in the text.
  * @param {string} text - The text to compare with the search.
- * @param {Array<Array<int>>} L - A computed LCS table.
- * @returns {lcsBacktrack} The result from backtracking our LCS table.
+ * @returns {Object} The result from backtracking our LCS table.
  */
-const lcsString = function (search, text, L) {
-  const searchLength = search.length;
+const lcsString = function (search, text) {
   const textLength = text.length;
   const strs = [];
-  let i = searchLength;
+  let i = search.length;
   let j = textLength;
   let prev = false;
   let longestSubstr = 0;
@@ -143,17 +136,13 @@ const lcsString = function (search, text, L) {
     strs.push(text.slice(0, j));
   }
 
-  strs.reverse();
-
-  const lcsObj = {
+  return {
     firstMatch: left,
     longestSubstr,
     subsequenceWidth: right - left + 1,
     subsequenceCount,
-    html: strs.join('')
+    html: strs.reverse().join('')
   };
-
-  return lcsObj;
 };
 
 /**
@@ -180,14 +169,14 @@ const lcs = function (search, text) {
     };
   }
 
-  const { L, length } = lcsTable(search, text.toLocaleLowerCase());
+  const length = lcsTable(search, text.toLocaleLowerCase());
   const {
     firstMatch,
     longestSubstr,
     subsequenceWidth,
     subsequenceCount,
     html
-  } = lcsString(search, text, L);
+  } = lcsString(search, text);
 
   return {
     length,
